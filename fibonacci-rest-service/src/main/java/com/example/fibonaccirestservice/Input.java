5e8b525c929cd06cebe23afebda82d6daedfe8b6
wanlingt <wanling210@gmail.com>
2021-07-04 16:18:50 +0800

    updated input parameters

--- fibonacci-rest-service/src/main/java/com/example/fibonaccirestservice/Input.java
+++ fibonacci-rest-service/src/main/java/com/example/fibonaccirestservice/Input.java
@@ -8,7 +8,9 @@ public class Input implements Serializable {
 	private Integer elements;
 
 	public Integer getElements() {
+		if (elements > 0 && elements <= 100)
 		return elements;
+		else return null;
 	}
 
 	public void setElements(Integer elements) {
